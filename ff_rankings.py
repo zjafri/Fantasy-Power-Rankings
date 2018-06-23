706a1fdd2c1844c8151674b068326f20fc1dd3e9
Zain <zjafri328@gmail.com>
2018-06-23 09:10:54 -0400

    Initial commit

---
+++ ff_rankings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 09:01:27 2018
+
+@author: Zain
+"""
+
+def main():
+	print("Hello World")
+ 
+if __name__ == '__main__':
+	main()
